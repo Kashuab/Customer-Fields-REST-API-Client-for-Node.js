f73d7ef50fe8f6f6a662aa120d54baf8a3830edf
Kyle Kashuba <kyle@heliumdev.com>
2021-01-19 12:08:55 -0800

    Updated interactive console to build the client if the lib folder does not exist

--- interactive-console.js
+++ interactive-console.js
@@ -1,3 +1,5 @@
+const fs = require('fs');
+const { exec } = require('child_process');
 const repl = require('repl');
 
 require('dotenv').config({ path: 'examples/.env' });
@@ -9,10 +11,27 @@ if (!process.env.CF_PRIVATE_ACCESS_TOKEN) {
   return;
 }
 
-globalThis.CF = require('./lib');
+(async () => {
+  if (!fs.existsSync('./lib')) {
+    console.log('Missing lib folder! Building client...');
+    await new Promise((resolve, reject) => {
+      exec('npm run build').on('exit', (code) => {
+        if (code != 0) {
+          reject('Failed to build client');
+        } else {
+          console.log('\n\nDone building client.\n\n');
+          resolve();
+        }
+      });
+    });
+  }
 
-console.log(
-  'You can reference the global CF variable to get started.\n',
-  'Try running: CF.Customer.find({}).then(([customers]) => console.log(customers.length));\n\n',
-);
-repl.start();
+  globalThis.CF = require('./lib');
+
+  console.log(
+    'You can reference the global CF variable to get started.\n',
+    'Try running: CF.Customer.find({}).then(([customers]) => console.log(customers.length));\n\n',
+  );
+
+  repl.start();
+})();
